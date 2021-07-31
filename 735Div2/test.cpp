a22346a6e577b110e6a8038cde9ead68e3a6e5ee
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-07-31 23:32:20 +0530

    735Div2

---
+++ 735Div2/test.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define endl "\n" 
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll t;
+	cin >> t;
+
+	while(t--){
+		ll n , m;
+		cin >> n >> m;
+
+		ll d = n^(m+1)
+	}
+
+	ll a =123456;
+	ll b =654322;
+	ll c =a^b;
+	cout<<c;
+
+}
