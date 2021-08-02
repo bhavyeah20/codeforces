5bf4ae3b93d4b8449a86d0581fc1e3ee487d4178
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-08-02 18:30:05 +0530

    736Div2

---
+++ 736Div2/A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define endl "\n" 
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll t;
+	cin >> t;
+
+	while(t--){
+
+		ll p;
+		cin >> p;
+		ll cnt = 0;
+		
+
+		cout<<2<<" "<<p-1;
+
+		cout<<endl;
+
+
+
+	}
+
+
+}
