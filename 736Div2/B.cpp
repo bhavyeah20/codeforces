fdd76a3bbe73a6279299589a622543179eb823f2
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-08-03 19:00:35 +0530

    736Div2

---
+++ 736Div2/B.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define endl "\n" 
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll t;
+	cin >> t;
+
+	while(t--){
+		ll n;
+		cin >> n;
+
+		string s1;
+		cin >> s1;
+		string s2;
+		cin >> s2;
+	}
+
+
+}
